b4cc76b8ea29b152be5592dc2c228d81990c9d47
diegogsilva97 <diegogsilva97@gmail.com>
2025-12-16 17:03:14 -0300

    Alterações

--- src/app/packages/package-list/package-list.ts
+++ src/app/packages/package-list/package-list.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PackageItem } from './package-item/package-item';
+import { Package } from './package.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-package-list',
-  imports: [PackageItem],
+  imports: [PackageItem,CommonModule],
   templateUrl: './package-list.html',
   styleUrl: './package-list.css',
 })
-export class PackageList {
+export class PackageList{
+  packages:Package[] = [
+    new Package('Churrasco', 'Churrasco de picanha', 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia7011/churrasco-cursos-cpt.jpg'),
+    new Package('Churrasco', 'Churrasco de picanha', 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia7011/churrasco-cursos-cpt.jpg')
+  ];
 
 }
